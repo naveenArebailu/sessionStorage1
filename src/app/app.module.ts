2879506614ca457148862b8f4ff4f737e7ab0b42
naveen arebailu <naveen.arebail@gmail.com>
2017-09-28 22:53:20 +0530

    files added

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { RootComponent } from './app.component';
+import { DropDownService } from './service/drop-down.service';
+import { CapitalPipe } from './capital.pipe';
+import { SearchPipe } from './search.pipe';
+import { ViewComponent } from './view/view.component';
+import { RoutesProvider } from './app.routes';
+import { AddComponent } from './add/add.component';
+
+@NgModule({
+  declarations: [
+    RootComponent,
+    CapitalPipe,
+    SearchPipe,
+    ViewComponent,
+    AddComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RoutesProvider
+  ],
+  providers: [DropDownService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [RootComponent]
+})
+export class AppModule { }
